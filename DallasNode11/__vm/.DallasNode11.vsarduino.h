65fd810b69ca5b1f1eead49f10de9189bfcb2dbd
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2016-01-21 00:03:59 +0100

    Added Temp/Motion/Battery-node with different wakeup-scripts

--- DallasNode11/__vm/.DallasNode11.vsarduino.h
+++ DallasNode11/__vm/.DallasNode11.vsarduino.h
@@ -85,4 +85,5 @@ extern "C" void __cxa_pure_virtual() {;}
 #define portInputRegister(P)
 #define portModeRegister(P)
 #include <..\DallasNode11\DallasNode11.ino>
+#include <..\DallasNode11\TempMotBattNode13.ino>
 #endif
